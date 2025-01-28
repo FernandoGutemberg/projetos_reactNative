86a1889baff618e5744d08507210c98e3814f38b
FernandoGutemberg <fernando70sil@gmail.com>
2025-01-28 10:38:30 -0300

    organizacao

--- MeuProjetinho/app/(tabs)/index.tsx
+++ MeuProjetinho/app/(tabs)/index.tsx
@@ -4,21 +4,21 @@ import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import * as Location from 'expo-location';
 
 const App = () => {
-  // Função para buscar a localização
+
   const fetchLocation = async () => {
     try {
-      // Solicitar permissão para acessar a localização
+
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         Alert.alert('Erro', 'Permissão para acessar localização foi negada');
         return;
       }
 
-      // Buscar a localização atual
+
       let currentLocation = await Location.getCurrentPositionAsync({});
       const { latitude, longitude } = currentLocation.coords;
 
-      // Exibir a localização no Alert
+
       Alert.alert(
         'Localização Atual',
         `Latitude: ${latitude}\nLongitude: ${longitude}`
@@ -35,7 +35,6 @@ const App = () => {
           <Text style={styles.title}>PEGA LOCALIZAÇÃO</Text>
           <Text style={styles.subtitle}>VAMOS PEGAR LOCALIZAÇÃO AQUI</Text>
           
-          {/* Botão para buscar localização */}
           <Button title="BUSCAR" onPress={fetchLocation} />
         </View>
       </SafeAreaView>
